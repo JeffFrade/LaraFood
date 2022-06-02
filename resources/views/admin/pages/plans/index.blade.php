24857fa17f55ba0a29b72c98b1cf1290b0d2691e
Jefferson Frade <jefferrson.frade@gmail.com>
2022-06-02 20:01:45 -0300

    Created plans index

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Planos')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Planos</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">Listagem dos Planos</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('admin/plans', 'Admin\PlanController@index')->name('plans.index');
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
